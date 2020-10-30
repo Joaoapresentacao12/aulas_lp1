6a79b2e1612f3876db24c2de08708312dccc03dd
joao <21152@alunos.ipca.pt>
2020-10-30 19:40:24 +0000

    first commit

---
+++ aula.c
@@ -0,0 +1,26 @@
+/**
+ * @file aula.c
+ * @author João Apresentação(21152@alunos.ipca.pt)
+ * @brief 
+ * @version 0.1
+ * @date 2020-10-27
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <stdio.h> 
+
+/**
+ * @brief programa que faz...
+ * 
+ * @return int 
+ */
+
+int main() 
+{
+    int x = 1; 
+    printf("%d\n", x); 
+
+}
+
